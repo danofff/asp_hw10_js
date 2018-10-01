0ad7a4f359cae5530c7604b2d08212da9f054432
dan <dan@DESKTOP-HHK66MJ>
2018-10-01 18:50:44 +0600

    last commit

---
+++ asp_hw10_js/Migrations/201810011248253_add_data_to_customers.cs
@@ -0,0 +1,31 @@
+namespace asp_hw10_js.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class add_data_to_customers : DbMigration
+    {
+        public override void Up()
+        {
+            Sql(@"
+SET IDENTITY_INSERT [dbo].[Customers] ON
+INSERT INTO [dbo].[Customers] ([Id], [Name], [Age], [Date], [EmploymentId]) VALUES (1, N'Tom', 44, N'2001-12-01 00:00:00', 1)
+INSERT INTO [dbo].[Customers] ([Id], [Name], [Age], [Date], [EmploymentId]) VALUES (2, N'Mary', 22, N'2002-12-02 00:00:00', 2)
+INSERT INTO [dbo].[Customers] ([Id], [Name], [Age], [Date], [EmploymentId]) VALUES (3, N'Jane', 28, N'2003-12-03 00:00:00', 3)
+INSERT INTO [dbo].[Customers] ([Id], [Name], [Age], [Date], [EmploymentId]) VALUES (4, N'Lex', 51, N'2004-10-12 00:00:00', 4)
+INSERT INTO [dbo].[Customers] ([Id], [Name], [Age], [Date], [EmploymentId]) VALUES (5, N'Paul', 23, N'2005-11-02 00:00:00', 5)
+INSERT INTO [dbo].[Customers] ([Id], [Name], [Age], [Date], [EmploymentId]) VALUES (6, N'Frank', 32, N'2002-08-19 00:00:00', 6)
+INSERT INTO [dbo].[Customers] ([Id], [Name], [Age], [Date], [EmploymentId]) VALUES (7, N'Joe', 30, N'2004-12-18 00:00:00', 7)
+INSERT INTO [dbo].[Customers] ([Id], [Name], [Age], [Date], [EmploymentId]) VALUES (8, N'George', 22, N'2007-09-02 00:00:00', 8)
+INSERT INTO [dbo].[Customers] ([Id], [Name], [Age], [Date], [EmploymentId]) VALUES (9, N'Zoe', 15, N'2006-08-19 00:00:00', 9)
+INSERT INTO [dbo].[Customers] ([Id], [Name], [Age], [Date], [EmploymentId]) VALUES (10, N'Hloe', 16, N'2008-12-18 00:00:00', 9)
+SET IDENTITY_INSERT [dbo].[Customers] OFF
+
+");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
